8b2b8e11c6844bfec332b4361389ce5d7ee123cb
Catherine Anozie <nayaanozie@gmail.com>
2024-03-13 04:06:28 +0000

    Upload page updated

--- src/pages/UPloadPage/Upload.js
+++ src/pages/UPloadPage/Upload.js
@@ -1,22 +1,41 @@
 import React from "react";
+import thumbnail from "../../assets/images/mohan-muruge.jpg";
 //import Navbar from "../../components/Header/Header";
 
 function Upload() {
   return (
     <>
-      <h1> Upload Video</h1>
-
       <hr />
-      <div className="upload__cotnainer">
-        <div className="upload__thumbnail"></div>
-        <div className="upload__details">
-          <form>
-            <input
-              className="login__input"
-              type="text"
-              placeholder="Please enter your name to log in"
-            />
-          </form>
+      <div className="upload__container">
+        <h1> Upload Video</h1>
+        <hr />
+        <div className="upload__content">
+          {/* <div className="upload__thumbnail-img"> */}
+          <img className="upload__thumbnail" src={thumbnail} alt="" />
+          {/* </div> */}
+          <div className="upload__details">
+            <form className="upload__form">
+              <label>TITLE YOUR VIDEO</label>
+              <input
+                className="upload__tittle-input"
+                type="text"
+                placeholder="Add a title to your video"
+              />
+              <label>ADD A VIDEO DESCRIPTION</label>
+              <textarea
+                className="upload__tittle-input"
+                type="text"
+                placeholder="Add a tittle"
+              ></textarea>
+            </form>
+          </div>
+        </div>
+        <hr />
+        <div className="upload__action">
+          <p>Cancel</p>
+          <button className="header__upload-button" type="submit">
+            Upload
+          </button>
         </div>
       </div>
     </>
